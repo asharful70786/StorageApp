d7ced7d31f2cdd7d26e2da9864f02c4952394cbb
Ashraful momin <ashrafulmomin7076@gmail.com>
2025-12-01 20:18:56 +0530

    fixed github signature verification

--- server/app.js
+++ server/app.js
@@ -12,24 +12,12 @@ import { connectDB } from "./config/db.js";
 import crypto from "crypto";
 import { exec } from "child_process";
 
-
-
-
 const app = express();
 await connectDB();
 const PORT = process.env.PORT || 4000;
 
 
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
-
-
-app.post(  "/github-webhook",  express.raw({ type: "application/json" }),  (req, res) => {
+app.post("/github-webhook",  express.raw({ type: "application/json" }),  (req, res) => {
     const signature = req.headers["x-hub-signature-256"];
     if (!signature) {
       console.error("Missing signature");
@@ -38,7 +26,7 @@ app.post(  "/github-webhook",  express.raw({ type: "application/json" }),  (req,
 
     const hmac = crypto
       .createHmac("sha256", process.env.git_webHook_sec)
-      .update(req.body)  
+      .update(req.body) 
       .digest("hex");
 
     const expectedSignature = `sha256=${hmac}`;
@@ -52,7 +40,6 @@ app.post(  "/github-webhook",  express.raw({ type: "application/json" }),  (req,
 
     console.log("Webhook verified — starting deploy...");
 
-
     exec("bash /home/ubuntu/client-deployment.sh", (err, stdout, stderr) => {
       if (err) return console.error("Frontend deploy error:", stderr);
       console.log("Frontend deployed:", stdout);
@@ -62,16 +49,21 @@ app.post(  "/github-webhook",  express.raw({ type: "application/json" }),  (req,
       if (err) return console.error("Backend deploy error:", stderr);
       console.log("Backend deployed:", stdout);
     });
-
   }
 );
 
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser(process.env.SESSION_SECRET));
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from my   StorageApp!" });
+  res.json({ message: "Hello from my personal  StorageApp!" });
 });
 
 app.get("/err", (req, res) => {
@@ -88,16 +80,13 @@ app.use("/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   console.log(err);
-  // res.status(err.status || 500).json({ error: "Something went wrong!" });
   res.json(err);
 });
 
-app.get("/err" , (req , res)=> {
-console.log('error occered');
-process.exit(1);
+app.get("/err", (req, res) => {
+  console.log("error occered");
+  process.exit(1);
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server Started ${PORT}`);
