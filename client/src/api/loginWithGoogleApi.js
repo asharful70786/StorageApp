d32454a9363d8f75062155af09418f8c7b72b3f3
Ashraful momin <ashrafulmomin7076@gmail.com>
2025-12-12 18:27:29 +0530

    moved clientSide Code to a Separate repo for smooth Ci/cd impliment

--- client/src/api/loginWithGoogleApi.js
+++
@@ -1,4 +0,0 @@
-export const loginWithGoogle = async (idToken) => {
-  const { data } = await axiosWithCreds.post("/auth/google", { idToken });
-  return data;
-};
--- client/src/context/DirectoryContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const DirectoryContext = createContext(null);
-
-export function useDirectoryContext() {
-  return useContext(DirectoryContext);
-}
